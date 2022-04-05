ce54cc248f23e5826dc59fd69436129e8aa2b90a
lili442 <103057016+lili442@users.noreply.github.com>
2022-04-05 20:11:20 +0200

    Update lili_app.py

--- lili_app.py
+++ lili_app.py
@@ -3,3 +3,5 @@ st.title("Form for the Users")
 st.write("Here, you can answer to some questions in this form.")
 st.write("We have to ask a few questions")
 user_id = st.text_input("ID", value="Your ID", max_chars=7)
+info = st.text_area("Share some information about you", "Put information here",
+                    help='You can write about your hobbies or family')
