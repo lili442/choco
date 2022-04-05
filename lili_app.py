b528af6442c6e5a9da1d808d9a1ebaff677dc8d2
lili442 <103057016+lili442@users.noreply.github.com>
2022-04-05 20:10:32 +0200

    Update lili_app.py

--- lili_app.py
+++ lili_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("Form for the Users")
 st.write("Here, you can answer to some questions in this form.")
 st.write("We have to ask a few questions")
+user_id = st.text_input("ID", value="Your ID", max_chars=7)
