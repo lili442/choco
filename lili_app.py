dbfe6b2224f4db55a1620afa9b7ac29608c249ed
lili442 <103057016+lili442@users.noreply.github.com>
2022-04-13 18:03:37 +0200

    Update lili_app.py

--- lili_app.py
+++ lili_app.py
@@ -5,3 +5,6 @@ st.write("We have to ask a few questions")
 user_id = st.text_input("ID", value="Your ID", max_chars=7)
 info = st.text_area("Share some information about you", "Put information here",
                     help='You can write about your hobbies or family')
+age = st.number_input("Age", min_value=18, max_value=100, step=1)
+birth_date = st.date_input("Date of Birth", min_value=datetime.date(1921, 1, 1),
+                           max_value=datetime.date(2003, 12, 31))
