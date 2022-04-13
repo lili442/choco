eb8dbbfe73852bb04645e452e7e254a21fa1dcc1
lili442 <103057016+lili442@users.noreply.github.com>
2022-04-13 18:08:11 +0200

    Update lili_app.py

--- lili_app.py
+++ lili_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-st.title("Form for the Users")
+st.title("Questions for students")
 st.write("Here, you can answer to some questions in this form.")
 st.write("We have to ask a few questions")
 user_id = st.text_input("ID", value="Your ID", max_chars=7)
 info = st.text_area("Share some information about you", "Put information here",
                     help='You can write about your hobbies or family')
-age = st.number_input("Age", min_value=18, max_value=100, step=1)
-birth_date = st.date_input("Date of Birth", min_value=date(1921, 1, 1),
-                           max_value=date(2003, 12, 31))
+age = st.number_input("Age", min_value=17, max_value=100, step=1)
+
+finance = st.checkbox("Do you study finance?")
+genre = st.radio("Which specialisation do you follow?",
+                 options=['Investment Banking', 'Financial Markets', 'Corporate Finance'])
